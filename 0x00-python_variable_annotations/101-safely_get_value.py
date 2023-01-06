ff670be501ffa9d1e9aa6df74579eefae4537dca
Str0-codes <danielMMomodu@gmail.com>
2023-01-06 01:14:21 +0100

    Update

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
+
 """Function with type annotations"""
 from typing import Mapping, Any, TypeVar, Union
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[None, TypeVar('T')]) -> Union[
+                     default: Union[TypeVar('T'), None]) -> Union[
                                                             Any,
                                                             TypeVar('T')]:
     """Returns key in a dictionary or default"""
@@ -12,11 +12,3 @@ def safely_get_value(dct: Mapping, key: Any,
         return dct[key]
     else:
         return default
-
-
-annotations = safely_get_value.__annotations__
-
-
-print("Here's what the mappings should look like")
-for k, v in annotations.items():
-    print(("{}: {}".format(k, v)))
