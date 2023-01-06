bf6ba672c2f38144573d25ca4213c65259139ef5
Str0-codes <danielMMomodu@gmail.com>
2023-01-06 01:02:32 +0100

    Update

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python3
 """ This scripts contains a functon that takes a list of floats
 and integers and adds them """
 import typing
@@ -7,10 +7,3 @@ import typing
 def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
     """returns float as a string"""
     return sum(mxd_lst)
-
-
-print(sum_mixed_list.__annotations__)
-mixed = [5, 4, 3.14, 666, 0.99]
-ans = sum_mixed_list(mixed)
-print(ans == sum(mixed))
-print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
